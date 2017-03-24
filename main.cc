d79fda97c5d59f96049d9e85ea6bf3738752b3af
Liu.Xuanqing <xuanqingliu@outlook.com>
2017-03-23 20:00:41 -0700

    add data loader

--- main.cc
+++ main.cc
@@ -1,42 +1,130 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
+#define MAX_TAG_DIGITS 16
+#define MAX_IDX_DIGITS 16
+#define MAX_VAL_DIGITS 16
 
 typedef struct Elements_ {
-	int colIdx;
+	long colIdx;
 	double value;
-} Elements;
+} Element;
 
 typedef struct DataRow_ {
-	int nLength;
-	Elements *elements;
+	int output;
+	long nLength;
+	Element *elements;
 } DataRow;
 
 typedef struct Data_ {
-	int nCols;
-	int nRows;
+	long nCols;
+	long nRows;
 	DataRow *dataRows;
-};
+} Data;
 
 // load data from file, make sure data = NULL
 void loadData(const char* file, Data* data) {
+  FILE *fHandle = fopen(file, "r");
+  if (fHandle == NULL) {
+    fprintf(stderr, "Fail to open file: %s\n", file);
+    data = NULL;
+    return;
+  }
+  // read whole file, then parse it
+  fseek(fHandle, 0, SEEK_END);
+  unsigned long fSize = ftell(fHandle);
+  fseek(fHandle, 0, SEEK_SET);
+  char *content = (char*)malloc(fSize * sizeof(char));
+  size_t count = fread(content, sizeof(char), fSize, fHandle);
+  assert((count == fSize));
 
+  //data = (Data*)malloc(sizeof(Data));
+  long nSample = 0;
+  long nFeature = 0;
+  unsigned long nElem = 0;
+  DataRow *dataRow = (DataRow*)malloc(sizeof(DataRow) * 1);
+  Element *elemBuffer = (Element*)malloc(sizeof(Element) * 1); // we estimate #elem by fSize / 16.
+  long rowBufferSize = 1; //1024
+  long elemBufferSize = 1; // fSize >> 4
+  int firstOutput = 0;
+  for (unsigned long i = 0; i < fSize; ++i) {
+    // allocate one row
+    if (nSample == rowBufferSize) {
+      // enlarge by 2 times
+      rowBufferSize <<= 1;
+      dataRow = (DataRow*)realloc(dataRow, sizeof(DataRow) * rowBufferSize);
+    }
+    DataRow *thisRow = dataRow + nSample;
+    nSample++;
+    // read class 
+    char tag[MAX_TAG_DIGITS + 1] = {0};
+    for(int tag_idx = 0; content[i] != ' ' && tag_idx < MAX_TAG_DIGITS; ++i, ++tag_idx) {
+      tag[tag_idx] = content[i];
+    }
+    if (firstOutput == 0) {
+      thisRow->output = 1;
+      firstOutput = atoi(tag);
+    }
+    else {
+      if (atoi(tag) == firstOutput) thisRow->output = 1;
+      else thisRow->output = -1;
+    }
+    while (content[i] == ' ') i++;
+    thisRow->nLength = 0;
+    // read elements
+    for(; content[i] != '\n';) {
+        if (nElem == elemBufferSize) {
+          elemBufferSize <<= 1;
+          elemBuffer = (Element*)realloc(elemBuffer, sizeof(Element) * elemBufferSize);
+        }
+        Element *thisElem = elemBuffer + nElem;
+        char indexStr[MAX_IDX_DIGITS + 1] = {0};
+        char valStr[MAX_VAL_DIGITS + 1] = {0};
+        for (int idx = 0; content[i] != ':'; ++i, ++idx) {
+          indexStr[idx] = content[i];
+        }
+        i += 1;
+        for (int idx = 0; content[i] != ' '; ++i, ++idx) {
+          valStr[idx] = content[i];
+        }
+        while (content[i] == ' ') i++;
+        thisElem->colIdx = atoi(indexStr) - 1;
+        if (thisElem->colIdx + 1 > nFeature) 
+          nFeature = thisElem->colIdx + 1;
+        thisElem->value = atof(valStr);
+        nElem += 1;
+        thisRow->nLength += 1;
+    }
+  }
+  // link elemBuffer with dataRow
+  for (int i = 0; i < nSample; ++i) {
+    dataRow[i].elements = elemBuffer;
+    elemBuffer += dataRow[i].nLength;
+  }
+  data->dataRows = dataRow;
+  data->nRows = nSample;
+  data->nCols = nFeature;
+  free(content);
+  fclose(fHandle);
 }
 
 // clean memory
 void rmData(Data* data) {
-
+  free(data->dataRows->elements);
+  free(data->dataRows);
 }
 
 // server side
 void server() {
-
+	//TODO
 }
 
 
 // client side
 void client(int clientId) {
-
+	//TODO
 }
 
 
@@ -48,7 +136,17 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	printf("I am %d of %d\n", rank + 1, size);
-
+  if (rank == 0) { // for server
+	  // load data
+		Data data;
+		loadData("../real-sim", &data);
+    printf("nRows: %ld, nFeature: %ld\n", data.nRows, data.nCols);
+		server();
+		rmData(&data);
+	}
+	else { // for clients
+		client(rank);
+	}
 	MPI_Finalize();
 	
 	return 0;
